bdcd0f52f10add1ca65ac1c2e6e6eea78a40c48a
naveenpallagani-altersquare <naveen.pallagani@altersquare.io>
2026-02-07 13:25:45 +0530

    Fix

--- config/http.js
+++ config/http.js
@@ -6,31 +6,8 @@ let bodyParser = require("body-parser").json({ limit: "50mb" });
 let compression = require("compression");
 let cors = require("cors");
 let cookieParser = require("cookie-parser");
-/* This whitelist can only filter requests from the browser clients */
-var whitelist = ["http://localhost:3000", "http://localhost:5173"];
 
-var corsOptions = {
-	origin: function (origin, callback) {
-		// console.log("HTTP Origin given = ", origin);
-		if (!origin) {
-			// console.log("origin undefined")
-			callback(null, true);
-		} else if (whitelist.indexOf(origin) !== -1) {
-			callback(null, true);
-		} else if (origin == null) {
-			// console.log("origin null")
-			callback(null, true);
-		} else if (origin.indexOf("chrome-extension") >= 0) {
-			callback(null, true);
-		} else {
-			console.log("[Not allowed by CORS] but allowed temporarily", origin);
-			callback(null, true);
-			// callback("Not allowed by CORS", false)
-			// callback(new Error("Not allowed by CORS"), false)
-		}
-		return;
-	},
-};
-let corsMiddle = cors(corsOptions);
+/* Allow all origins for now */
+let corsMiddle = cors();
 
 module.exports = [bodyParser, compression(), corsMiddle, cookieParser()];
